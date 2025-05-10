eafe0218c9c1d2c2b326729291a2cb0c56b01fe9
Muhammed Shabeel <mshabeel999@gmail.com>
2025-05-10 13:53:44 +0530

    news ui updated

--- lib/app/widgets/news/news_card.dart
+++ lib/app/widgets/news/news_card.dart
@@ -18,62 +18,69 @@ class NewsItemWidgetIOS extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        // color: CupertinoColors.systemBackground,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              newsItem.title,
-              style: const TextStyle(
-                fontSize: 17,
-                fontWeight: FontWeight.w600,
-                color: Color.fromARGB(255, 255, 255, 255),
-                letterSpacing: -0.3,
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Container(
+          decoration: BoxDecoration(
+color: const Color.fromARGB(53, 255, 255, 255),
+borderRadius: BorderRadius.circular(15)
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                newsItem.title,
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  letterSpacing: -0.3,
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
               ),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(height: 4),
-            Text(
-              DateFormat('MMM dd, yyyy').format(newsItem.createdAt),
-              style: const TextStyle(
-                fontSize: 14,
-                color: Color.fromARGB(153, 190, 190, 190),
+              const SizedBox(height: 4),
+              Text(
+                DateFormat('MMM dd, yyyy').format(newsItem.createdAt),
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Color.fromARGB(153, 190, 190, 190),
+                ),
               ),
-            ),
-            const SizedBox(height: 6),
-            Text(
-              newsItem.description,
-              style: const TextStyle(
-                fontSize: 15,
-                color: Color.fromARGB(153, 190, 190, 190),
-                height: 1.3,
+              const SizedBox(height: 6),
+              Text(
+                newsItem.description,
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Color.fromARGB(153, 190, 190, 190),
+                  height: 1.3,
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
               ),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: const [
-                Text(
-                  'Read more',
-                  style: TextStyle(
-                    fontSize: 14,
+              const SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Text(
+                    'Read more',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 190, 190, 190),
+                    ),
+                  ),
+                  SizedBox(width: 4),
+                  Icon(
+                    CupertinoIcons.chevron_right,
+                    size: 14,
                     color: Color.fromARGB(255, 190, 190, 190),
                   ),
-                ),
-                SizedBox(width: 4),
-                Icon(
-                  CupertinoIcons.chevron_right,
-                  size: 14,
-                  color: Color.fromARGB(255, 190, 190, 190),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
